1ab049cffb34d0938a271e9c9c09d4ace9429fb9
Fabrício <fabaguiarsilva@gmail.com>
2021-11-24 12:14:29 -0200

    commit inicial do projeto

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: fabriciosilva
+ *
+ * Created on November 24, 2021, 12:13 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    return (EXIT_SUCCESS);
+}
+
