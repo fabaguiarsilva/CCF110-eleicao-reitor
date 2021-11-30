b956b4d3d329b6b1b0a796fded28c19833294d85
Fabrício <fabaguiarsilva@gmail.com>
2021-11-30 10:28:05 -0200

    Primeira versão, sem condicional. Apenas mostra os votos de cada eleitor.

--- main.c
+++ main.c
@@ -12,7 +12,49 @@
  * 
  */
 int main() {
-    int nCandidatos = 3;
+    /*
+     * Inicialmente, apenas 3 candidatos a reitor
+     */
+    int nVotosCandidato1 = 0;
+    int nVotosCandidato2 = 0;
+    int nVotosCandidato3 = 0;
+    
+    /*
+     * Inicialmente, apenas 5 eleitores
+     */
+    int votoEleitor1,votoEleitor2,votoEleitor3,
+            votoEleitor4,votoEleitor5;
+    
+    printf("Digite:\n");
+    printf("1 - para votar no candidato 1\n");
+    printf("2 - para votar no candidato 2\n");
+    printf("3 - para votar no candidato 3\n");
+    printf("*************************\n\n");
+    
+    printf("Qual o voto do eleitor 1?\n");
+    scanf("%d",&votoEleitor1);
+    
+    printf("Qual o voto do eleitor 2?\n");
+    scanf("%d",&votoEleitor2);
+    
+    printf("Qual o voto do eleitor 3?\n");
+    scanf("%d",&votoEleitor3);
+    
+    printf("Qual o voto do eleitor 4?\n");
+    scanf("%d",&votoEleitor4);
+    
+    printf("Qual o voto do eleitor 5?\n");
+    scanf("%d",&votoEleitor5);
+    
+    printf("O eleitor 1 votou em: %d\n",votoEleitor1);
+    printf("O eleitor 2 votou em: %d\n",votoEleitor2);
+    printf("O eleitor 3 votou em: %d\n",votoEleitor3);
+    printf("O eleitor 4 votou em: %d\n",votoEleitor4);
+    printf("O eleitor 5 votou em: %d\n",votoEleitor5);
+    
+    
+    printf("\n\nFavor calcular o resultado manualmente!");
+    
     return (EXIT_SUCCESS);
 }
 
